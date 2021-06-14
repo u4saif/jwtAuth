16ff89c03408a4ac8a6de04cd07e01e46a6200ee
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-06-14 12:46:54 +0530

    fix env variable

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express=require('express');
 const jwt=require("jsonwebtoken");
 const port=process.env.PORT || 3000;
 const app=express();
+const bodyParser= require("body-parser");
+app.use(bodyParser.json());
 
-
+require("dotenv/config");
 app.get('/api',(req,res)=>{
     res.status(200).send("project running...");
 });
 
+//Allowing API access if the token is varified true
 app.post('/api/post',checkTokenExist,(req,res)=>{
-    jwt.verify(req.token,"secrateKey",(err,auth)=>{
+    jwt.verify(req.token,process.env.secrateKey,(err,auth)=>{
         if(err){
             res.status(403).send("Forbidden inValid token");
         }else{
@@ -22,14 +25,16 @@ app.post('/api/post',checkTokenExist,(req,res)=>{
   
 });
 
+//Generating a JWT token for perticular user login session
 app.post("/api/login",(req,res)=>{
     //Test User 
-    let user={
-        "username":"saif",
-        "id":1,
-        "email":"u4saif@gmail.com"
-    }
-    jwt.sign({user:user},'secrateKey',{expiresIn: '12sec'},(err,token)=>{
+    // let user={
+    //     "username":"saif",
+    //     "id":1,
+    //     "email":"u4saif@gmail.com"
+    // }
+    console.log(req.body);
+    jwt.sign(req.body,process.env.secrateKey,{expiresIn: '120sec'},(err,token)=>{
         res.json({
             token:token
         });
